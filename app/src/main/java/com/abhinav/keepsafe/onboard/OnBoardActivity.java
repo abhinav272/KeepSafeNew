f9d05664e3effad800773333633f5c15574b9cf5
Abhinav Sharma <abhinav.ih@gmail.com>
2017-04-23 10:13:46 +0530

    OnBoardActivity added

---
+++ app/src/main/java/com/abhinav/keepsafe/onboard/OnBoardActivity.java
@@ -0,0 +1,20 @@
+package com.abhinav.keepsafe.onboard;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.abhinav.keepsafe.BaseActivity;
+import com.abhinav.keepsafe.R;
+
+/**
+ * Created by Abhinav on 23/04/17.
+ */
+public class OnBoardActivity extends BaseActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboard);
+    }
+}
