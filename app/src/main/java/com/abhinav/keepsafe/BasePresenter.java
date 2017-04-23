0c233c28811a8b524ecf639c89bd99679489a506
Abhinav Sharma <abhinav.ih@gmail.com>
2017-04-23 16:34:51 +0530

    BasePresenter added

---
+++ app/src/main/java/com/abhinav/keepsafe/BasePresenter.java
@@ -0,0 +1,13 @@
+package com.abhinav.keepsafe;
+
+/**
+ * Created by Abhinav on 23/04/17.
+ */
+public interface BasePresenter {
+
+    /**
+     * call start() when fragment or activity is about to interact with user
+     * this method provides functionality like fetch default data etc.
+     */
+    void start();
+}
