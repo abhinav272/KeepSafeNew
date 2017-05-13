c0804b973f71631f10f96b4943cc506886298f55
Abhinav Sharma <abhinav.ih@gmail.com>
2017-05-13 20:59:29 +0530

    Login Fragment created

---
+++ app/src/main/java/com/abhinav/keepsafe/login/LoginFragment.java
@@ -0,0 +1,148 @@
+package com.abhinav.keepsafe.login;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.abhinav.keepsafe.BaseFragment;
+import com.abhinav.keepsafe.R;
+import com.andrognito.pinlockview.IndicatorDots;
+import com.andrognito.pinlockview.PinLockListener;
+import com.andrognito.pinlockview.PinLockView;
+
+/**
+ * Created by Abhinav on 23/04/17.
+ */
+public class LoginFragment extends BaseFragment implements LoginContract.IView {
+
+    private LoginContract.IPresenter mPresenter;
+
+    private PinLockView mPinLockView;
+    private IndicatorDots mIndicatorDots;
+    private TextView tvBanner;
+    private String pin1, pin2;
+    private Context context;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        mPresenter = new LoginPresenter();
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initView(view);
+        setupLockView(view);
+    }
+
+    private void initView(View view) {
+        tvBanner = (TextView) view.findViewById(R.id.tv_banner);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        this.context = context;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mPresenter.start();
+    }
+
+    @Override
+    public void setPresenter(LoginContract.IPresenter presenter) {
+        mPresenter = checkNotNull(presenter);
+        tvBanner.setText(getString(R.string.choose_pin_string));
+    }
+
+    @Override
+    public void showLoginSuccessMessage() {
+        Toast.makeText(context, getString(R.string.login_success), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void showPinSavedSuccessfullyMessage() {
+        mPresenter.saveNewPin(pin1);
+        Toast.makeText(context, getString(R.string.pin_saved_successfully), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void showInvalidPinMessage() {
+        mPinLockView.resetPinLockView();
+        Toast.makeText(context, getString(R.string.invalid_pin_msg), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void showPinMismatchMessage() {
+        Toast.makeText(context, getString(R.string.pin_mismatch), Toast.LENGTH_SHORT).show();
+        mPinLockView.resetPinLockView();
+        pin1 = pin2 = null;
+        tvBanner.setText(getString(R.string.choose_pin_string));
+    }
+
+    @Override
+    public void setupLockView(View view) {
+        mPinLockView = (PinLockView) view.findViewById(R.id.pin_lock_view);
+        mIndicatorDots = (IndicatorDots) view.findViewById(R.id.indicator_dots);
+        tvBanner.setText(getString(R.string.choose_pin_string));
+        if (mIndicatorDots != null) {
+            mIndicatorDots.setPinLength(4);
+        }
+        mPinLockView.attachIndicatorDots(mIndicatorDots);
+
+        mPinLockView.setPinLockListener(new PinLockListener() {
+            @Override
+            public void onComplete(String pin) {
+                Log.e("onComplete: ", pin);
+                if(!mPresenter.isAlreadyOnBoarded()){
+                    if (pin1 == null) {
+                        pin1 = pin;
+                        tvBanner.setText(R.string.confirm_pin_string);
+                        mPinLockView.resetPinLockView();
+                    } else{
+                        pin2 = pin;
+                        if(pin1.equals(pin2)){
+                            showPinSavedSuccessfullyMessage();
+                            navigateToHome();
+                        } else {
+                            showPinMismatchMessage();
+                        }
+                    }
+                } else {
+                    if(mPresenter.validatePin(pin)){
+                        showLoginSuccessMessage();
+                        navigateToHome();
+                    }
+                    else {
+                        showInvalidPinMessage();
+                    }
+                }
+            }
+
+            @Override
+            public void onEmpty() {
+                Log.e("onEmpty: ", "");
+            }
+
+            @Override
+            public void onPinChange(int pinLength, String intermediatePin) {
+                Log.e("onPinChange: ", intermediatePin + "  " + pinLength);
+            }
+        });
+    }
+
+    @Override
+    public void navigateToHome() {
+
+    }
+}
