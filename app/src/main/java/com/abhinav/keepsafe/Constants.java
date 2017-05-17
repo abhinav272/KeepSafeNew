b31fe9e5701db52d1ddc9db3a86326fd39ae3142
Abhinav Sharma <abhinav.ih@gmail.com>
2017-05-17 22:08:52 +0530

    constants file created

---
+++ app/src/main/java/com/abhinav/keepsafe/Constants.java
@@ -0,0 +1,20 @@
+package com.abhinav.keepsafe;
+
+/**
+ * Created by Abhinav on 17/05/17.
+ */
+public final class Constants {
+
+    public static final class ExtrasKey{
+        public static final String INTENT_EXTRA_BUNDLE = "intent_extra_bundle";
+    }
+
+    public static final class NavigateTo{
+        public static final int HOME = 1;
+    }
+
+    public static final class LaunchedBy {
+        public static final String LOGIN = "login";
+        public static final String HOME = "home";
+    }
+}
