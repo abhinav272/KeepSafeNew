491afcd6eabebb85d601bc65183eb9f7a2e02624
Abhinav Sharma <abhinav.ih@gmail.com>
2017-05-20 10:34:52 +0530

    LoginFragment entry dicided

--- app/src/main/java/com/abhinav/keepsafe/onboard/OnBoardActivity.java
+++ app/src/main/java/com/abhinav/keepsafe/onboard/OnBoardActivity.java
@@ -5,6 +5,7 @@ import android.support.annotation.Nullable;
 
 import com.abhinav.keepsafe.BaseActivity;
 import com.abhinav.keepsafe.R;
+import com.abhinav.keepsafe.login.LoginFragment;
 
 /**
  * Created by Abhinav on 23/04/17.
@@ -16,5 +17,6 @@ public class OnBoardActivity extends BaseActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboard);
+        addFragment(getFragmentManager(), new LoginFragment(), R.id.frame_container, LoginFragment.class.getSimpleName());
     }
 }
