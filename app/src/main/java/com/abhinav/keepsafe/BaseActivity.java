9a53a0f18fd9c32a9678eea04bfce84aa76e9d68
Abhinav Sharma <abhinav.ih@gmail.com>
2017-05-20 10:36:42 +0530

    Base methods for adding fragments added in base activity

--- app/src/main/java/com/abhinav/keepsafe/BaseActivity.java
+++ app/src/main/java/com/abhinav/keepsafe/BaseActivity.java
@@ -1,9 +1,36 @@
 package com.abhinav.keepsafe;
 
 import android.app.Activity;
+import android.app.FragmentManager;
 
 /**
  * Created by Abhinav on 23/04/17.
  */
 public class BaseActivity extends Activity {
+
+    public void addFragmentWithBackStack(FragmentManager fragmentManager, BaseFragment baseFragment, int containerResId, String tag) {
+        fragmentManager.beginTransaction()
+                .add(containerResId, baseFragment, tag)
+                .addToBackStack(tag)
+                .commit();
+    }
+
+    public void addFragment(FragmentManager fragmentManager, BaseFragment baseFragment, int containerResId, String tag) {
+        fragmentManager.beginTransaction()
+                .add(containerResId, baseFragment, tag)
+                .commit();
+    }
+
+    public void replaceFragmentWithBackStack(FragmentManager fragmentManager, BaseFragment baseFragment, int containerResId, String tag) {
+        fragmentManager.beginTransaction()
+                .replace(containerResId, baseFragment, tag)
+                .addToBackStack(tag)
+                .commit();
+    }
+
+    public void replaceFragment(FragmentManager fragmentManager, BaseFragment baseFragment, int containerResId, String tag) {
+        fragmentManager.beginTransaction()
+                .replace(containerResId, baseFragment, tag)
+                .commit();
+    }
 }
