65f8f665384f709cb49c82e12e17f2b4d5947b95
Abhinav Sharma <abhinav.ih@gmail.com>
2017-05-20 10:37:11 +0530

    common methods for showing message added

--- app/src/main/java/com/abhinav/keepsafe/BaseFragment.java
+++ app/src/main/java/com/abhinav/keepsafe/BaseFragment.java
@@ -1,9 +1,30 @@
 package com.abhinav.keepsafe;
 
 import android.app.Fragment;
+import android.support.design.widget.Snackbar;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 /**
  * Created by Abhinav on 23/04/17.
  */
 public class BaseFragment extends Fragment {
+
+    public <T> T checkNotNull(T presenter) {
+        if (presenter == null) {
+            throw new NullPointerException("IPresenter cannot be null");
+        } else return presenter;
+    }
+
+    public void showMessageOnSnackBar(String msg){
+        Snackbar.make(null, msg, Snackbar.LENGTH_LONG).show();
+    }
+
+    public void showToast(String msg){
+        Toast.makeText(getActivity(), msg, Toast.LENGTH_SHORT).show();
+    }
+
+    public void setupToolbar(Toolbar toolbar){
+        getActivity().setActionBar(toolbar);
+    }
 }
