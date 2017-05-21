a0a9f49bc930a841667e773a2f09316476cd5a53
Abhinav Sharma <abhinav.ih@gmail.com>
2017-05-21 18:57:40 +0530

    candidate pojo added

---
+++ app/src/main/java/com/abhinav/keepsafe/pojo/Candidate.java
@@ -0,0 +1,12 @@
+package com.abhinav.keepsafe.pojo;
+
+/**
+ * Created by Abhinav on 20/05/17.
+ */
+public class Candidate {
+
+    public int mCandidateId;
+    public String mCandidateName;
+    public int mCategoryId;
+    public String mCandidatePassword;
+}
