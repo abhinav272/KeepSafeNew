2cd79fa361558e79f33adb510fce39cdf5714a87
Abhinav Sharma <abhinav.ih@gmail.com>
2017-10-07 19:28:04 +0530

    conflict strategy added

--- app/src/main/java/com/abhinav/keepsafe/dao/BankDao.java
+++ app/src/main/java/com/abhinav/keepsafe/dao/BankDao.java
@@ -4,6 +4,7 @@ import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
 import android.arch.persistence.room.Query;
 import android.arch.persistence.room.Update;
 
@@ -25,7 +26,7 @@ public interface BankDao {
     @Query("SELECT * FROM " + Constants.Database.T_BANK + "WHERE id = :id")
     LiveData<Bank> getBankDetails(int id);
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     void addBank(Bank bank);
 
     @Update
--- app/src/main/java/com/abhinav/keepsafe/dao/EmailDao.java
+++ app/src/main/java/com/abhinav/keepsafe/dao/EmailDao.java
@@ -4,6 +4,7 @@ import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
 import android.arch.persistence.room.Query;
 import android.arch.persistence.room.Update;
 
@@ -25,7 +26,7 @@ public interface EmailDao {
     @Query("SELECT * FROM " + Constants.Database.T_EMAIL + "WHERE id = :id")
     LiveData<Email> getEmailDetails(int id);
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     void addEmail(Email email);
 
     @Update
