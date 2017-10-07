dc20dbf2491aa68741739bad5aa1cd688b5959b9
Abhinav Sharma <abhinav.ih@gmail.com>
2017-10-07 14:18:24 +0530

    bank dao created

---
+++ app/src/main/java/com/abhinav/keepsafe/dao/BankDao.java
@@ -0,0 +1,36 @@
+package com.abhinav.keepsafe.dao;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import com.abhinav.keepsafe.Constants;
+import com.abhinav.keepsafe.entities.Bank;
+
+import java.util.List;
+
+/**
+ * Created by abhinav.sharma on 07/10/17.
+ */
+
+@Dao
+public interface BankDao {
+
+    @Query("SELECT * FROM " + Constants.Database.T_BANK)
+    LiveData<List<Bank>> getAllBanks();
+
+    @Query("SELECT * FROM " + Constants.Database.T_BANK + "WHERE id = :id")
+    LiveData<Bank> getBankDetails(int id);
+
+    @Insert
+    void addBank(Bank bank);
+
+    @Update
+    void updateBank(Bank bank);
+
+    @Delete
+    void deleteBank(Bank bank);
+}
