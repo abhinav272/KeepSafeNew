b27d1d24759992a5709a466e1cea10704c1cfa85
Abhinav Sharma <abhinav.ih@gmail.com>
2017-10-09 22:03:50 +0530

    login model listener created for async support

---
+++ app/src/main/java/com/abhinav/keepsafe/login/LoginModelListener.java
@@ -0,0 +1,11 @@
+package com.abhinav.keepsafe.login;
+
+import com.abhinav.keepsafe.base.BaseModelListener;
+
+/**
+ * Created by abhinav.sharma on 09/10/17.
+ */
+
+interface LoginModelListener extends BaseModelListener {
+
+}
