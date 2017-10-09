d1167781676a40141049ecf5820242318f1fad43
Abhinav Sharma <abhinav.ih@gmail.com>
2017-10-09 22:03:27 +0530

    login model created

---
+++ app/src/main/java/com/abhinav/keepsafe/login/LoginModel.java
@@ -0,0 +1,14 @@
+package com.abhinav.keepsafe.login;
+
+import com.abhinav.keepsafe.base.BaseModel;
+
+/**
+ * Created by abhinav.sharma on 09/10/17.
+ */
+
+public class LoginModel extends BaseModel<LoginModelListener> {
+
+    public LoginModel(LoginModelListener listener) {
+        super(listener);
+    }
+}
