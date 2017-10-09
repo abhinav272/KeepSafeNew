b631954ec3b81a1b2107a4a25a51dc8e0cc41eed
Abhinav Sharma <abhinav.ih@gmail.com>
2017-10-09 22:04:21 +0530

    base fragment updated

--- app/src/main/java/com/abhinav/keepsafe/BaseFragment.java
+++ app/src/main/java/com/abhinav/keepsafe/BaseFragment.java
@@ -5,30 +5,43 @@ import android.support.design.widget.Snackbar;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
+import com.abhinav.keepsafe.base.*;
+
 /**
  * Created by Abhinav on 23/04/17.
  */
-public class BaseFragment extends Fragment {
+public class BaseFragment extends Fragment implements com.abhinav.keepsafe.base.IBaseView {
 
-    public <T> T checkNotNull(T presenter) {
-        if (presenter == null) {
-            throw new NullPointerException("IPresenter cannot be null");
-        } else return presenter;
-    }
-
-    public void showMessageOnSnackBar(String msg){
-        Snackbar.make(null, msg, Snackbar.LENGTH_LONG).show();
-    }
-
-    public void showToast(String msg){
-        Toast.makeText(getActivity(), msg, Toast.LENGTH_SHORT).show();
-    }
-
-    public void setupToolbar(Toolbar toolbar){
+    public void setupToolbar(Toolbar toolbar) {
         getActivity().setActionBar(toolbar);
     }
 
     public void setToolbarTitle(int titleResId) {
         getActivity().getActionBar().setTitle(titleResId);
+    }
+
+    @Override
+    public void showSnackbar(String msg) {
+        Snackbar.make(null, msg, Snackbar.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void showToastLong(String msg) {
+        Toast.makeText(getActivity(), msg, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void showToastShort(String msg) {
+        Toast.makeText(getActivity(), msg, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void showProgressDialog() {
+
+    }
+
+    @Override
+    public void hideProgressDialog() {
+
     }
 }
