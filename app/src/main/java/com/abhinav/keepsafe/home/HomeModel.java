29a6a82698597cc304413f213ecbd5c5e58b2f12
Abhinav Sharma <abhinav.ih@gmail.com>
2017-10-11 21:03:48 +0530

    Home Model created and listener attached for future async calls

---
+++ app/src/main/java/com/abhinav/keepsafe/home/HomeModel.java
@@ -0,0 +1,14 @@
+package com.abhinav.keepsafe.home;
+
+import com.abhinav.keepsafe.base.BaseModel;
+
+/**
+ * Created by abhinav.sharma on 11/10/17.
+ */
+
+class HomeModel extends BaseModel<HomeModelListener>{
+
+    public HomeModel(HomeModelListener listener) {
+        super(listener);
+    }
+}
