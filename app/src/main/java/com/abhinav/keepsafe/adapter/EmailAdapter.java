980313ed6d9145414773a12dd1468a85adea8b9b
Abhinav Sharma <abhinav.ih@gmail.com>
2017-10-13 14:16:53 +0530

    email adapter and view holder created

---
+++ app/src/main/java/com/abhinav/keepsafe/adapter/EmailAdapter.java
@@ -0,0 +1,48 @@
+package com.abhinav.keepsafe.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.abhinav.keepsafe.R;
+import com.abhinav.keepsafe.adapter.viewholder.EmailViewHolder;
+import com.abhinav.keepsafe.entities.Email;
+
+import java.util.List;
+
+/**
+ * Created by abhinav.sharma on 13/10/17.
+ */
+
+public class EmailAdapter extends RecyclerView.Adapter<EmailViewHolder> {
+
+    private LayoutInflater inflater;
+    private List<Email> emails;
+
+    public EmailAdapter(Context context, List<Email> emails) {
+        this.emails = emails;
+        inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public EmailViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.layout_single_text_view, parent, false);
+        return new EmailViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(EmailViewHolder holder, int position) {
+        holder.tvName.setText(getItem(position).getEmailId());
+    }
+
+    @Override
+    public int getItemCount() {
+        return emails.size();
+    }
+
+    private Email getItem(int position) {
+        return emails.get(position);
+    }
+}
