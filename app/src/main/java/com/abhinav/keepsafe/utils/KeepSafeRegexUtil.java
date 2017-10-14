d12115126c7015d82c0b094c10d173f2226f8a24
Abhinav Sharma <abhinav.ih@gmail.com>
2017-10-14 19:46:07 +0530

    regex util updated for valid email and phone number

--- app/src/main/java/com/abhinav/keepsafe/utils/KeepSafeRegexUtil.java
+++ app/src/main/java/com/abhinav/keepsafe/utils/KeepSafeRegexUtil.java
@@ -1,8 +1,26 @@
 package com.abhinav.keepsafe.utils;
 
+import android.util.Patterns;
+
+import com.abhinav.keepsafe.Constants;
+
+import java.util.regex.Pattern;
+
 /**
  * Created by abhinav.sharma on 13/10/17.
  */
 
 public class KeepSafeRegexUtil {
+
+    public static boolean isValidEmail(CharSequence emailId) {
+        return Patterns.EMAIL_ADDRESS.matcher(emailId).matches();
+    }
+
+    public static boolean isValidPhoneNumber(CharSequence phone){
+        String phoneGarbage = "[()\\s-\\s+]+";
+        String phoneRegex = "\\d{10}";
+        phone = phone.toString().replaceAll(phoneGarbage, "");
+        Pattern pattern = Pattern.compile(phoneRegex);
+        return phone != null && pattern.matcher(phone).matches() && phone.length() == 10;
+    }
 }
