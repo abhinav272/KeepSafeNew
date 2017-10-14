9e74db5bdc256dce72da5fd984580950a7b3c0c7
Abhinav Sharma <abhinav.ih@gmail.com>
2017-10-14 19:54:40 +0530

    ecommerce adpater created

---
+++ app/src/main/java/com/abhinav/keepsafe/adapter/ECommerceAdapter.java
@@ -0,0 +1,48 @@
+package com.abhinav.keepsafe.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.abhinav.keepsafe.R;
+import com.abhinav.keepsafe.adapter.viewholder.ECommerceViewHolder;
+import com.abhinav.keepsafe.entities.ECommerce;
+
+import java.util.List;
+
+/**
+ * Created by abhinav.sharma on 13/10/17.
+ */
+
+public class ECommerceAdapter extends RecyclerView.Adapter<ECommerceViewHolder> {
+
+    private List<ECommerce> eCommerces;
+    private LayoutInflater inflater;
+
+    public ECommerceAdapter(Context context, List<ECommerce> eCommerces) {
+        this.eCommerces = eCommerces;
+        inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public ECommerceViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.layout_single_text_view, parent, false);
+        return new ECommerceViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ECommerceViewHolder holder, int position) {
+        holder.tvName.setText(getItem(position).getPlatformName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return eCommerces.size();
+    }
+
+    private ECommerce getItem(int position) {
+        return eCommerces.get(position);
+    }
+}
