ade09daaf28bf146a9fbde1082f78351d22ffab6
Abhinav Sharma <abhinav.ih@gmail.com>
2017-10-14 19:56:00 +0530

    socialnetworkviewholder created

---
+++ app/src/main/java/com/abhinav/keepsafe/adapter/viewholder/SocialNetworkViewHolder.java
@@ -0,0 +1,21 @@
+package com.abhinav.keepsafe.adapter.viewholder;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.abhinav.keepsafe.R;
+
+/**
+ * Created by abhinav.sharma on 13/10/17.
+ */
+
+public class SocialNetworkViewHolder extends RecyclerView.ViewHolder{
+
+    public TextView tvName;
+
+    public SocialNetworkViewHolder(View itemView) {
+        super(itemView);
+        tvName = itemView.findViewById(R.id.tv_name);
+    }
+}
