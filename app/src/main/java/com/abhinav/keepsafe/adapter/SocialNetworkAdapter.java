813811f69623214b9d239ce13b662708c62a9895
Abhinav Sharma <abhinav.ih@gmail.com>
2017-10-14 19:55:24 +0530

    socialnetwork adpater created

---
+++ app/src/main/java/com/abhinav/keepsafe/adapter/SocialNetworkAdapter.java
@@ -0,0 +1,48 @@
+package com.abhinav.keepsafe.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.abhinav.keepsafe.R;
+import com.abhinav.keepsafe.adapter.viewholder.SocialNetworkViewHolder;
+import com.abhinav.keepsafe.entities.SocialNetwork;
+
+import java.util.List;
+
+/**
+ * Created by abhinav.sharma on 13/10/17.
+ */
+
+public class SocialNetworkAdapter extends RecyclerView.Adapter<SocialNetworkViewHolder>{
+
+    private List<SocialNetwork> socialNetworks;
+    private LayoutInflater inflater;
+
+    public SocialNetworkAdapter(Context context, List<SocialNetwork> socialNetworks) {
+        this.socialNetworks = socialNetworks;
+        inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public SocialNetworkViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.layout_single_text_view, parent, false);
+        return new SocialNetworkViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(SocialNetworkViewHolder holder, int position) {
+        holder.tvName.setText(getItem(position).getPlatformName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return socialNetworks.size();
+    }
+
+    private SocialNetwork getItem(int position) {
+        return socialNetworks.get(position);
+    }
+}
