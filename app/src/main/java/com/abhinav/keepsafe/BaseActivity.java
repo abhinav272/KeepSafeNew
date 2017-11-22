8c8ee9712c9c35e4e7fd404280267c28b866a779
Abhinav Sharma <abhinav.ih@gmail.com>
2017-11-22 13:22:32 +0530

    Toolbar methods changed

--- app/src/main/java/com/abhinav/keepsafe/BaseActivity.java
+++ app/src/main/java/com/abhinav/keepsafe/BaseActivity.java
@@ -2,11 +2,12 @@ package com.abhinav.keepsafe;
 
 import android.app.Activity;
 import android.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by Abhinav on 23/04/17.
  */
-public class BaseActivity extends Activity {
+public class BaseActivity extends AppCompatActivity {
 
     public void addFragmentWithBackStack(FragmentManager fragmentManager, BaseFragment baseFragment, int containerResId, String tag) {
         fragmentManager.beginTransaction()
