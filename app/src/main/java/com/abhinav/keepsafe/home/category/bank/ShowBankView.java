57d04e2eeff588c05f449fdc26754b547f42cf4f
Abhinav Sharma <abhinav.ih@gmail.com>
2017-11-25 14:41:06 +0530

    ShowFragment MVP created

---
+++ app/src/main/java/com/abhinav/keepsafe/home/category/bank/ShowBankView.java
@@ -0,0 +1,13 @@
+package com.abhinav.keepsafe.home.category.bank;
+
+import com.abhinav.keepsafe.base.IBaseView;
+import com.abhinav.keepsafe.entities.Bank;
+
+/**
+ * Created by abhinav.sharma on 24/11/17.
+ */
+
+interface ShowBankView extends IBaseView{
+    void showBankDetails(Bank bank);
+    void showEditBankFragment(int bankId);
+}
