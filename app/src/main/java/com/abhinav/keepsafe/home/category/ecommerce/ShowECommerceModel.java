7e86b680bebc6e76e7b03ffaa0048f44e226077d
Abhinav Sharma <abhinav.ih@gmail.com>
2017-11-25 22:04:47 +0530

    New UI and flow for Ecommerce

---
+++ app/src/main/java/com/abhinav/keepsafe/home/category/ecommerce/ShowECommerceModel.java
@@ -0,0 +1,15 @@
+package com.abhinav.keepsafe.home.category.ecommerce;
+
+import com.abhinav.keepsafe.base.BaseModel;
+import com.abhinav.keepsafe.base.BaseModelListener;
+
+/**
+ * Created by abhinav.sharma on 25/11/17.
+ */
+
+class ShowECommerceModel extends BaseModel<BaseModelListener> {
+
+    public ShowECommerceModel(BaseModelListener listener) {
+        super(listener);
+    }
+}
