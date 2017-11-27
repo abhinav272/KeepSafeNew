996c4c040db465cddf7ecd0267c06df066dc04c4
Abhinav Sharma <abhinav.ih@gmail.com>
2017-11-27 16:32:27 +0530

    new UI and coordinator animation added for SocialNetwork

--- app/src/main/java/com/abhinav/keepsafe/home/category/socialnetwork/AddSocialNetworkFragment.java
+++ app/src/main/java/com/abhinav/keepsafe/home/category/socialnetwork/AddSocialNetworkFragment.java
@@ -2,12 +2,21 @@ package com.abhinav.keepsafe.home.category.socialnetwork;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TextInputLayout;
+import android.support.v4.view.ViewCompat;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewStub;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ImageView;
 
 import com.abhinav.keepsafe.BaseFragment;
 import com.abhinav.keepsafe.R;
@@ -19,40 +28,81 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 import butterknife.Unbinder;
 
+import static com.abhinav.keepsafe.Constants.Defaults.PERCENTAGE_TO_SHOW_IMAGE;
+
 /**
  * Created by abhinav.sharma on 13/10/17.
  */
 
-public class AddSocialNetworkFragment extends BaseFragment implements AddSocialNetworkView {
+public class AddSocialNetworkFragment extends BaseFragment implements AddSocialNetworkView, AppBarLayout.OnOffsetChangedListener {
 
-    @BindView(R.id.tv_social_network_logo)
-    TextView tvSocialNetworkLogo;
-    @BindView(R.id.tv_social_network_platform_name)
-    EditText tvSocialNetworkPlatformName;
-    @BindView(R.id.tv_social_network_password)
-    EditText tvSocialNetworkPassword;
-    @BindView(R.id.tv_email_id_used)
-    EditText tvEmailIdUsed;
-    @BindView(R.id.tv_social_network_user_name)
-    EditText tvSocialNetworkUserName;
-    @BindView(R.id.tv_save)
-    TextView tvSave;
+
     Unbinder unbinder;
     AddSocialNetworkPresenter presenter;
+    @BindView(R.id.iv_header)
+    ImageView ivHeader;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.collapsing)
+    CollapsingToolbarLayout collapsing;
+    @BindView(R.id.appbar)
+    AppBarLayout appbar;
+    @BindView(R.id.vs_add_category_item)
+    ViewStub vsAddCategoryItem;
+    @BindView(R.id.cardview)
+    CardView cardview;
+    @BindView(R.id.fab)
+    FloatingActionButton fab;
+    @BindView(R.id.coordinator)
+    CoordinatorLayout coordinator;
+    private TextInputLayout til_social_network_platform_name, til_email_id_used,
+            til_social_network_password, til_social_network_user_name;
+    private EditText et_social_network_platform_name, et_email_id_used, et_social_network_password,
+            et_social_network_user_name;
+    private int mMaxScrollSize;
+    private boolean mIsImageHidden;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_add_social_network, container, false);
+        View view = inflater.inflate(R.layout.fragment_add_category_item, container, false);
         unbinder = ButterKnife.bind(this, view);
         presenter = new AddSocialNetworkPresenter(this);
+        initCoordinatorView();
         return view;
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        inflateSocialNetworkViewStub();
+        initSocialNetworkStubView(view);
+    }
 
+    private void initCoordinatorView() {
+        setupToolbar(toolbar);
+        toolbar.setTitle(null);
+        appbar.addOnOffsetChangedListener(this);
+        ivHeader.setImageResource(R.drawable.img_social_header);
+        collapsing.setTitle("Add Social Network Details");
+    }
+
+    private void inflateSocialNetworkViewStub() {
+        vsAddCategoryItem.setInflatedId(R.id.layout_social_network_details);
+        vsAddCategoryItem.setLayoutResource(R.layout.fragment_add_social_network);
+        vsAddCategoryItem.inflate();
+    }
+
+    private void initSocialNetworkStubView(View view) {
+        til_social_network_password = view.findViewById(R.id.til_social_network_password);
+        til_social_network_platform_name = view.findViewById(R.id.til_social_network_platform_name);
+        til_social_network_user_name = view.findViewById(R.id.til_social_network_user_name);
+        til_email_id_used = view.findViewById(R.id.til_email_id_used);
+
+        et_social_network_password = view.findViewById(R.id.et_social_network_password);
+        et_social_network_platform_name = view.findViewById(R.id.et_social_network_platform_name);
+        et_social_network_user_name = view.findViewById(R.id.et_social_network_username);
+        et_email_id_used = view.findViewById(R.id.et_email_id_used);
     }
 
     @Override
@@ -62,33 +112,33 @@ public class AddSocialNetworkFragment extends BaseFragment implements AddSocialN
         presenter.detachView();
     }
 
-    @OnClick(R.id.tv_save)
+    @OnClick(R.id.fab)
     public void onViewClicked() {
         if (fireValidations()) {
             SocialNetwork socialNetwork = new SocialNetwork();
-            socialNetwork.setPlatformName(tvSocialNetworkPlatformName.getText().toString().trim());
-            socialNetwork.setEmailIdUsed(tvEmailIdUsed.getText().toString().trim());
-            socialNetwork.setUsername(tvSocialNetworkUserName.getText().toString().trim());
-            socialNetwork.setPassword(tvSocialNetworkPassword.getText().toString().trim());
+            socialNetwork.setPlatformName(et_social_network_platform_name.getText().toString().trim());
+            socialNetwork.setEmailIdUsed(et_email_id_used.getText().toString().trim());
+            socialNetwork.setUsername(et_social_network_user_name.getText().toString().trim());
+            socialNetwork.setPassword(et_social_network_password.getText().toString().trim());
             presenter.addSocialNetwork(socialNetwork);
         }
     }
 
     private boolean fireValidations() {
         boolean flag = true;
-        if (TextUtils.isEmpty(tvSocialNetworkPlatformName.getText().toString().trim())) {
-            tvSocialNetworkPlatformName.setError(getString(R.string.invalid_social_platform));
+        if (TextUtils.isEmpty(et_social_network_platform_name.getText().toString().trim())) {
+            til_social_network_platform_name.setError(getString(R.string.invalid_social_platform));
             flag = false;
         }
 
-        if (TextUtils.isEmpty(tvEmailIdUsed.getText().toString().trim()) ||
-                !KeepSafeRegexUtil.isValidEmail(tvEmailIdUsed.getText().toString().trim())) {
-            tvEmailIdUsed.setError(getString(R.string.invalid_email));
+        if (TextUtils.isEmpty(et_email_id_used.getText().toString().trim()) ||
+                !KeepSafeRegexUtil.isValidEmail(et_email_id_used.getText().toString().trim())) {
+            til_email_id_used.setError(getString(R.string.invalid_email));
             flag = false;
         }
 
-        if (TextUtils.isEmpty(tvSocialNetworkPassword.getText().toString().trim())) {
-            tvSocialNetworkPassword.setError(getString(R.string.invalid_email_password));
+        if (TextUtils.isEmpty(et_social_network_password.getText().toString().trim())) {
+            til_social_network_password.setError(getString(R.string.invalid_email_password));
             flag = false;
         }
 
@@ -98,5 +148,28 @@ public class AddSocialNetworkFragment extends BaseFragment implements AddSocialN
     @Override
     public void popFragment() {
         getFragmentManager().popBackStackImmediate();
+    }
+
+    @Override
+    public void onOffsetChanged(AppBarLayout appBarLayout, int i) {
+        if (mMaxScrollSize == 0)
+            mMaxScrollSize = appBarLayout.getTotalScrollRange();
+
+        int currentScrollPercentage = (Math.abs(i)) * 100 / mMaxScrollSize;
+
+        if (currentScrollPercentage >= PERCENTAGE_TO_SHOW_IMAGE) {
+            if (!mIsImageHidden) {
+                mIsImageHidden = true;
+                ViewCompat.animate(fab).scaleY(0).scaleX(0).start();
+
+            }
+        }
+
+        if (currentScrollPercentage < PERCENTAGE_TO_SHOW_IMAGE) {
+            if (mIsImageHidden) {
+                mIsImageHidden = false;
+                ViewCompat.animate(fab).scaleY(1).scaleX(1).start();
+            }
+        }
     }
 }
